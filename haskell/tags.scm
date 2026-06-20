13ae6f62d3e3433021c7d613d84bea6e4342c3c3
Ching Pei Yang <badnam3o.0@gmail.com>
2026-06-21 00:00:49 +0200

    haskell: fix queries for record fields

--- haskell/tags.scm
+++ haskell/tags.scm
@@ -7,25 +7,42 @@
     name: (_) @name) @definition.class
 
 ; ADT constructor definitions
-(data_constructors
-    (data_constructor
-      (prefix name: (_) @name) @definition.function))
+(data_type
+  constructors: (data_constructors
+    constructor: (data_constructor
+      constructor: [
+        (record name: (_) @name) @definition.function
+        (prefix name: (_) @name) @definition.function
+      ])))
 
-; Record constructor definitions
-((record
-    constructor: (constructor) @name) @definition.function)
+; ADT record fields
+(data_type
+  constructors: (data_constructors
+    constructor: (data_constructor
+      constructor: (record
+        fields: (fields
+          field: (field
+            name: (field_name (variable) @name) @definition.field))))))
+
+; Newtype type name definition
+(newtype
+  name: (name) @name) @definition.class
+
+; Newtype constructor definition
+(newtype_constructor
+   name: (constructor) @name @definition.function)
+
+; Newtype record field
+(newtype
+  constructor: (newtype_constructor
+    field: (record
+      field: (field name: (_) @name) @definition.field)))
 
 ; GADT constructor definitions
 
 (gadt_constructors
     (gadt_constructor
       (constructor) @name) @definition.function)
-
-; Record field definitions
-(record
-	fields: (fields
-		field: (field
-			name: (field_name (variable)) @name) @definition.field))
 
 ; Type synonym definitions
 (type_synomym
@@ -47,6 +64,12 @@
 ; Class definitions
 (class
     name: (_) @name) @definition.interface
+
+(class
+  declarations: (class_declarations
+    declaration: (signature
+      name: (variable) @name) @definition.method))
+
 
 ; Module definitions
 (header
